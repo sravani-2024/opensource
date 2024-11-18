8f7e4eda90b1a5d5ebddba55e4b655257be5d98d
sravani-2024 <156399655+sravani-2024@users.noreply.github.com>
2024-11-18 15:44:50 +0530

    Find Age.py

---
+++ visaa_prep/Find Age.py
@@ -0,0 +1,10 @@
+def vignesh_age(charan_age):
+    
+    age_difference = 25 - 10
+    vignesh_age = charan_age - age_difference
+    return vignesh_age
+
+charan_age = int(input())
+
+result = vignesh_age(charan_age)
+print(result)
